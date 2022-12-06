d702e6095435d6f3360633751c51cfdbee815aa1
chenyyxx <chenyx.eric2013@gmail.com>
2022-12-05 19:12:18 -0600

    update data pipeline to filter conditions by pregnancy period

--- sample_patient_facing_app/gen_cond_var.js
+++ sample_patient_facing_app/gen_cond_var.js
@@ -69,20 +69,90 @@ if (maritalStatus.toLowerCase()=="single") {
 }
 
 
-console.log(feature_dict);
+// console.log(feature_dict);
 
-export default async function parseCondInfo(xml) {
+// function to extract EncounterId
+function getConditionEncounterId(entry) {
+    return entry.resource.Condition.encounter.reference._attributes.value;
+}
+
+
+// query social history in observation to get pregnancy start date
+var pregnancyStatus_xml = fs.readFileSync('./PregnancyStatus.xml', 'utf8')
+function getPregnancyStart(xml) {
+    var pregnancyStatus = JSON.parse(convert.xml2json(xml, {compact: true, spaces: 4}));
+    var entryList = pregnancyStatus.Bundle.entry
+    var startDate = ""
+    // var startDates = []
+    for(let i = 0; i < entryList.length; i++){
+        var observation = entryList[i].resource.Observation
+        var observation_coding = observation.code.coding
+        if (observation_coding.length > 1){
+            observation_coding = observation_coding.filter(o => o.system._attributes.value === "http://loinc.org")[0]
+        }
+        if(observation_coding.code._attributes.value === '82810-3'){
+            var pregancyStatusResult = observation.valueCodeableConcept.text
+            if(pregancyStatusResult._attributes.value === "Yes"){
+                // what if being pregnant multiple times?
+                startDate = observation.issued._attributes.value
+                // startDates.push(observation.effectiveDateTime._attributes.value) 
+            }
+        }
+    }
+    // console.log(startDate)
+    return startDate
+}
+// getPregnancyStart(pregnancyStatus_xml)
+
+// query vital signs in observation to get the pregancy end date
+var vitalsigns_xml = fs.readFileSync('./vitalsigns.xml', 'utf8')
+function getPregnancyEnd(xml){
+    var vitalsigns = JSON.parse(convert.xml2json(xml, {compact: true, spaces: 4}));
+    var endDate = vitalsigns.Bundle.entry.resource.Observation.issued._attributes.value
+    // console.log(endDate)
+    return endDate
+}
+// getPregnancyEnd(vitalsigns_xml)
+
+
+// function to parse the Encounter API response and get the encounter period
+// TODO: what if peiod is missing as it is optional
+var encounter_xml = fs.readFileSync('./encounter.xml', 'utf8');
+function getEncounterPeriod(xml) {
+    var encounterResponse = convert.xml2json(xml, {compact: true, spaces: 4});
+    var encounterRes = JSON.parse(encounterResponse);
+    var period = encounterRes.Bundle.entry.resource.Encounter.period.start._attributes.value;
+    // console.log(period)
+    return period;
+}
+// getEncounterPeriod(encounter_xml)
+
+// temporary
+function queryEncounterAPI(encounterId){
+    return encounter_xml
+}
+
+
+// xml should be conditions
+export default async function parseCondInfo(xml, pregnancyStart, pregnancyEnd) {
     var result1 = convert.xml2json(xml, {compact: true, spaces: 4});
     var res = JSON.parse(result1);
 
     // console.log(res.Bundle.entry);   
     var entryList = res.Bundle.entry;
-    var conditions = []
+    // var conditions = []
+    var conditionsDate = []
     for (let i = 0; i < entryList.length; i++) {
         var code = entryList[i].resource.Condition.code.coding[1].code._attributes.value;
-        conditions.push(code);
+        // conditions.push(code); // try changing this to a map => {code, date}
+        var encounterId = getConditionEncounterId(entryList[i]);
+        // request encounter API
+        var encounterXML = queryEncounterAPI(encounterId);
+        console.log(encounterXML)
+        var period = getEncounterPeriod(encounterXML)
+        conditionsDate.push({code: code, period: period});
     }
-    // console.log(conditions);
+    console.log(conditions);
 
 
     const code_json = fs.readFileSync('code_dict.json', 'utf-8');
@@ -92,8 +162,12 @@ export default async function parseCondInfo(xml) {
     var feature_dict = {}
     for (let i = 0; i < Object.keys(code_dict).length; i++) {
         var code = Object.keys(code_dict)[i];
-        if (conditions.includes(code)) {
-            feature_dict[code_dict[code]] = 1;
+        if (conditionsDate.map(o => o.code).includes(code)) {
+            var conditionObj = conditionsDate.find(o => o.code === code);
+            var conditionDate = conditionObj.period;
+            if (conditionDate >= pregnancyStart && conditionDate <= pregnancyEnd){
+                feature_dict[code_dict[code]] = 1;
+            }
         }
     }
 
